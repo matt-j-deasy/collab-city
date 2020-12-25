aaf953dbeec51b53bea63f1477eafd8eeb9de7f2
michael-oppenheimer <76538596+michael-oppenheimer@users.noreply.github.com>
2020-12-25 15:03:08 -0500

    Basic Styling for Blog
    Added working name of blog, center aligned everything. Added border/box shadow, new font, new color on h2s and h4s. Messed with letter-spacing and font-weights.

--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "C:/Users/Michael Oppenheimer/Documents/GitHub/collab-city/src/components/App/fonts.css"
 import { BlogPost as BlogPostType } from "../../util/types";
 import "./styles.css";
 
